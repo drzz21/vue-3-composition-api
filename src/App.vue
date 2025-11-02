9ff42bc4def0bcb957df66c2b3166cc70517cd9a
Francisco Diosdado <drz.y2k@gmail.com>
2025-11-01 21:05:54 -0600

    Vue Composition API Setup Method

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+	setup() {
+		const name = 'The Snazzy Burger';
+
+		const placeOrder = () => {
+			alert(`Order placed at ${name}!`);
+		};
+
+		return { placeOrder, name };
+	},
+};
+</script>
+
+<template>
+	<h1>{{ name }}</h1>
+	<button @click="placeOrder">Place Order</button>
+</template>
+
+<style scoped>
+.logo {
+	height: 6em;
+	padding: 1.5em;
+	will-change: filter;
+	transition: filter 300ms;
+}
+.logo:hover {
+	filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+	filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
+
