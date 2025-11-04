05188895fc87447aeed315955f3c4d74ca26fda1
Francisco Diosdado <drz.y2k@gmail.com>
2025-11-03 18:23:25 -0600

    The Setup Method's Parameters: props and context

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <script>
+import YummyMeal from './components/YummyMeal.vue';
+
 // tenemos que exportar por defecto de esta forma
 export default {
+	// declaramos nuestro componente para poder utilizarlo
+	components: {
+		YummyMeal,
+	},
 	//en vue 3 inicializamos nuestro js con la funcion setup
 	//donde creamos las constantes, funciones, etc
 	setup() {
@@ -10,8 +16,14 @@ export default {
 			alert(`Order placed at ${name}!`);
 		};
 
+		//creamos la funcion para imprimir el item agregado
+
+		const addItemToCart = (itemName) => {
+			alert(`Added ${itemName} to cart!`);
+		};
+
 		//para usarlo en nuestro código lo tenemos que retornar
-		return { placeOrder, name };
+		return { placeOrder, name, addItemToCart };
 	},
 };
 </script>
@@ -19,6 +31,10 @@ export default {
 <template>
 	<h1>{{ name }}</h1>
 	<button @click="placeOrder">Place Order</button>
+	<!-- llamamos la funcion dentro de nuestro evento emitido, y ahi recibimos
+	 el nombre del producto que enviamos desde la funcion dentro del componente
+	 aqui tambien enviamos las props al componente hijo -->
+	<YummyMeal name="Burger" :price="8.99" @add-to-cart="addItemToCart" />
 </template>
 
 <style scoped>
---
+++ src/components/YummyMeal.vue
@@ -0,0 +1,34 @@
+<template>
+	<p>
+		<strong>{{ name }}</strong> - ${{ price }}
+
+		<button @click="addToCart">Add to Cart</button>
+
+	</p>
+</template>
+
+<script>
+export default {
+	//de esta forma declaramos las props que recibiremos y el tipo de dato que serán
+	props: {
+		name: String,
+		price: Number,
+	},
+	//recibimos las props como argumento de nuestro setup
+	// y el segundo argumento tiene opciones como attrs, slots y emit
+	//podemos desestructurar el segundo argumento para obtener solo lo que necesitamos
+	//pero no el primero ya que perderiamos la reactividad
+	setup(props, { attrs, slots, emit }) {
+		//imprimimos el valor de las props
+		console.log(props.name);
+		console.log(props.price);
+
+		//emiitmos un evento al hacer click en el boton
+		const addToCart = () => {
+			emit('add-to-cart', props.name);
+		};
+		//retornamos para poder usar en el template
+		return { addToCart };
+	},
+};
+</script>
